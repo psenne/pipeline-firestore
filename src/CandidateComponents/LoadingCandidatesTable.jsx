33d5c03dd06b0e872b50c9126467e954e00d752b
psenne <psenne@mjparker.com>
2021-03-15 23:39:20 -0400

    loadedcandidates template takes an argument of rows

--- src/CandidateComponents/LoadingCandidatesTable.jsx
+++ src/CandidateComponents/LoadingCandidatesTable.jsx
@@ -1,97 +1,33 @@
 import React from "react";
-import { Placeholder, Container, Segment } from "semantic-ui-react";
+import { Placeholder, Segment } from "semantic-ui-react";
 
-export default function LoadingCandidatesTable() {
-    return (
-        <Container fluid className="hovered">
-            <div className="candidate-table-row">
-                <Segment attached padded>
-                    <Placeholder>
-                        <Placeholder.Header>
-                            <Placeholder.Line />
-                            <Placeholder.Line />
-                        </Placeholder.Header>
-                        <Placeholder.Paragraph>
-                            <Placeholder.Line />
-                            <Placeholder.Line />
-                            <Placeholder.Line />
-                            <Placeholder.Line />
-                            <Placeholder.Line />
-                        </Placeholder.Paragraph>
-                        <Placeholder.Paragraph>
-                            <Placeholder.Line />
-                            <Placeholder.Line />
-                            <Placeholder.Line />
-                        </Placeholder.Paragraph>
-                    </Placeholder>
-                </Segment>
-            </div>
-            <div className="candidate-table-row">
-                <Segment attached padded>
-                    <Placeholder>
-                        <Placeholder.Header>
-                            <Placeholder.Line />
-                            <Placeholder.Line />
-                        </Placeholder.Header>
-                        <Placeholder.Paragraph>
-                            <Placeholder.Line />
-                            <Placeholder.Line />
-                            <Placeholder.Line />
-                            <Placeholder.Line />
-                            <Placeholder.Line />
-                        </Placeholder.Paragraph>
-                        <Placeholder.Paragraph>
-                            <Placeholder.Line />
-                            <Placeholder.Line />
-                            <Placeholder.Line />
-                        </Placeholder.Paragraph>
-                    </Placeholder>
-                </Segment>
-            </div>
-            <div className="candidate-table-row">
-                <Segment attached padded>
-                    <Placeholder>
-                        <Placeholder.Header>
-                            <Placeholder.Line />
-                            <Placeholder.Line />
-                        </Placeholder.Header>
-                        <Placeholder.Paragraph>
-                            <Placeholder.Line />
-                            <Placeholder.Line />
-                            <Placeholder.Line />
-                            <Placeholder.Line />
-                            <Placeholder.Line />
-                        </Placeholder.Paragraph>
-                        <Placeholder.Paragraph>
-                            <Placeholder.Line />
-                            <Placeholder.Line />
-                            <Placeholder.Line />
-                        </Placeholder.Paragraph>
-                    </Placeholder>
-                </Segment>
-            </div>
-            <div className="candidate-table-row">
-                <Segment attached padded>
-                    <Placeholder>
-                        <Placeholder.Header>
-                            <Placeholder.Line />
-                            <Placeholder.Line />
-                        </Placeholder.Header>
-                        <Placeholder.Paragraph>
-                            <Placeholder.Line />
-                            <Placeholder.Line />
-                            <Placeholder.Line />
-                            <Placeholder.Line />
-                            <Placeholder.Line />
-                        </Placeholder.Paragraph>
-                        <Placeholder.Paragraph>
-                            <Placeholder.Line />
-                            <Placeholder.Line />
-                            <Placeholder.Line />
-                        </Placeholder.Paragraph>
-                    </Placeholder>
-                </Segment>
-            </div>
-        </Container>
-    );
+export default function LoadingCandidatesTable({ numrows }) {
+    numrows = numrows || 4;
+    const rows = [];
+
+    for (var i = 0; i < numrows; i++) {
+        rows.push(
+            <Segment key={i} padded>
+                <Placeholder>
+                    <Placeholder.Header>
+                        <Placeholder.Line />
+                        <Placeholder.Line />
+                    </Placeholder.Header>
+                    <Placeholder.Paragraph>
+                        <Placeholder.Line />
+                        <Placeholder.Line />
+                        <Placeholder.Line />
+                        <Placeholder.Line />
+                        <Placeholder.Line />
+                    </Placeholder.Paragraph>
+                    <Placeholder.Paragraph>
+                        <Placeholder.Line />
+                        <Placeholder.Line />
+                        <Placeholder.Line />
+                    </Placeholder.Paragraph>
+                </Placeholder>
+            </Segment>
+        );
+    }
+    return rows;
 }
