722cc22c186daf67799396e2a659595c01843ee4
MJP <psenne@mjparker.com>
2022-06-25 18:10:43 -0400

    finished contract update functions

--- functions/contracts.js
+++ functions/contracts.js
@@ -22,6 +22,38 @@ exports.updateContractName = functions.database.ref("/contracts/{id}/name").onUp
             });
         });
 
+    fsdb.collection("submissions")
+        .where("contract", "==", oldname)
+        .get()
+        .then(docs => {
+            docs.forEach(doc => {
+                doc.ref.update({ contract: newname });
+            });
+        });
+
+    fsdb.collection("employees")
+        .where("current_contract", "==", oldname)
+        .get()
+        .then(docs => {
+            docs.forEach(doc => {
+                doc.ref.update({ current_contract: newname });
+            });
+        });
+
+    fsdb.collection("candidates")
+        .where("potential_contracts", "array-contains", oldname)
+        .get()
+        .then(docs => {
+            docs.forEach(doc => {
+                const potential_contracts = doc.data().potential_contracts;
+                if (potential_contracts.includes(oldname)) {
+                    const idx = potential_contracts.indexOf(oldname);
+                    potential_contracts[idx] = newname;
+                    doc.ref.update({ potential_contracts: potential_contracts });
+                }
+            });
+        });
+
     rlt.ref("auditing")
         .push(event)
         .then(() => {
